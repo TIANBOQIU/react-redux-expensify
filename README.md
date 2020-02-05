4f6679d1dbbec86a0be65ed8225557e96e0d2d5c
= <doublelockcup@gmail.com>
2020-02-05 18:53:52 -0500

    adding sorting by filter params

--- README.md
+++ README.md
@@ -3,12 +3,13 @@
 > log
 
 1. #----#
-2. redux action, reducer and selector
-3. set up redux store
-4. Frontend routing in React Router
-5. use historyApiFallback: true for react router
-6. /public/index.html && /src/app.js
-7. set up project director structure
-8. edit package.json && yarn install && .gitignore
-9. yarn init && set up webpack.config && set up .babelrc
-10. create the project
+2. connect React Component with Redux store
+3. redux action, reducer and selector
+4. set up redux store
+5. Frontend/client-side routing in React Router
+6. use historyApiFallback: true for react router
+7. /public/index.html && /src/app.js
+8. set up project director structure
+9. edit package.json && yarn install && .gitignore
+10. yarn init && set up webpack.config && set up .babelrc
+11. create the project
---
+++ src/selectors/expenses.js
@@ -0,0 +1,30 @@
+/**
+ * selector for store:expenses
+ * filtering and sorting
+ */
+
+const getVisibleExpenses = (
+  expenses,
+  { searchText, sortBy, startDate, endDate }
+) => {
+  return expenses
+    .filter(expense => {
+      const startDateMatch =
+        typeof startDate === "number" || expense.createdAt >= startDate;
+      const endDateMatch =
+        typeof endDate === "number" || expense.createdAt <= endDate;
+      const textMatch = expense.description
+        .toLowerCase()
+        .includes(searchText.toLowerCase());
+      return startDateMatch && endDateMatch && textMatch;
+    })
+    .sort((a, b) => {
+      if (sortBy === "date") {
+        return b.createdAt - a.createdAt;
+      } else if (sortBy === "amount") {
+        return b.amount - a.amount;
+      }
+    });
+};
+
+export default getVisibleExpenses;
