852fe12e24192a1ac23f6b29310fcba6ef35d647
= <doublelockcup@gmail.com>
2020-02-05 15:16:00 -0500

    add Header

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -5,9 +5,11 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 // header, home page, add, edit, help, 404 Component
 import HomePage from "../pages/HomePage";
 import NotFoundPage from "../pages/NotFoundPage";
+import Header from "../Header";
 const AppRouter = () => (
   <BrowserRouter>
     <div>
+      <Header />
       <Switch>
         <Route path="/" component={HomePage} exact={true} />
         <Route component={NotFoundPage} />
