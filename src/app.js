715035978b1ae8eec0a5f23701d6d9f6b73028aa
= <doublelockcup@gmail.com>
2020-02-05 15:55:37 -0500

    connect React Component with Redux store

--- src/app.js
+++ src/app.js
@@ -4,8 +4,29 @@ import ReactDOM from "react-dom";
 // React Router
 import AppRouter from "./components/routers/AppRouter";
 
+// Redux store
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+
 // CSS
 import "normalize.css";
 // import "./styles/styles.scss";
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const store = configureStore();
+// bootstrap data
+store.dispatch(
+  addExpense({ description: "water bill", amount: 300, createdAt: 100 })
+);
+store.dispatch(
+  addExpense({ description: "gas bill", amount: 500, createdAt: 300 })
+);
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
